b462645fd76ab1714d471a07cd2b052394fe9429
hiroki minagawa <hiroki6805@gmail.com>
2018-12-12 01:15:46 +0900

    一番マッチした人間が表示されるようにしました。

--- compatibility-calculator.js
+++ compatibility-calculator.js
@@ -1,9 +1,32 @@
 const { candidates } = require('./data/candidates-data.js');
 
-const calculateBestMatch = function(quizSubmissions) {
+const caluclatePoint = (submittedAnswerBlock, candidateAnswerBlock, candidateScoreInfo) => {
+  for (let i = 0, len = submittedAnswerBlock.length; i < len; i += 1) {
+    const submittedAnswer = submittedAnswerBlock[i].value;
+    const candidateAnswer = candidateAnswerBlock[i].value;
+    if (submittedAnswer === candidateAnswer) {
+      candidateScoreInfo.score += 2;
+    } else if (Math.abs((Number(submittedAnswer) - Number(candidateAnswer))) === 1) {
+      candidateScoreInfo.score += 1;
+    }
+  }
+  return candidateScoreInfo;
+};
 
+const candidateMatched = (name) => {
+  let matchedPerson = [];
+  for (let i = 0, len = candidates.length; i < len; i += 1) {
+    const candidateName = candidates[i][0][0].value;
+    if (candidateName === name) {
+      matchedPerson = candidates[i];
+    }
+  }
+  return matchedPerson;
+};
+
+const calculateBestMatch = function(quizSubmissions) {
   // TODO: replace this...
-  return candidates[0];
+  // return candidates[0];
 
   // TODO: implement the following algorithm for
   // calculating the candidate who is the best match for you
@@ -21,6 +44,35 @@ const calculateBestMatch = function(quizSubmissions) {
   // for ties, the last person who has the highest score is returned
 
   // return closest match
+
+  const scoreStored = [];
+  const startQuestionBlock = 2;
+  const finishQuestionBlock = 6;
+
+  for (let i = 0; i < candidates.length; i += 1) {
+    let candidateScoreInfo = {};
+    const candidateName = candidates[i][0][0].value;
+    candidateScoreInfo.name = candidateName;
+    candidateScoreInfo.score = 0;
+    candidateScoreInfo.candidateNumber = i;
+    for (let t = startQuestionBlock; t <= finishQuestionBlock; t += 1) {
+      const submittedAnswerBlock = quizSubmissions[t];
+      const candidateAnswerBlock = candidates[i][t];
+      candidateScoreInfo = caluclatePoint(submittedAnswerBlock, candidateAnswerBlock, candidateScoreInfo);
+    }
+
+    scoreStored.push(candidateScoreInfo);
+  }
+
+  scoreStored.sort((a, b) => {
+    if (a.score < b.score) return 1;
+    if (a.score > b.score) return -1;
+    if (a.candidateNumber < b.candidateNumber) return 1;
+    if (a.candidateNumber > b.candidateNumber) return -1;
+    return 0;
+  });
+
+  return candidateMatched(scoreStored[0].name);
 };
 
 module.exports = {
